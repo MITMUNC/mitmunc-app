6a0881870d314910fb6475a76bbb256dd82c07ea
Karunya Sethuraman <karunyas@mit.edu>
2018-01-01 12:13:54 -0800

    map icon

--- main.lua
+++ main.lua
@@ -37,10 +37,10 @@ end
 
 -- table to setup buttons
 local tabButtons = {
-	{ label="Home", defaultFile="home.png", overFile="home.png", width = 32, height = 32, onPress=onHome, selected=true },
+	{ label="Home", defaultFile="home.png", overFile="home-down.png", width = 32, height = 32, onPress=onHome, selected=true },
 	{ label="Schedule", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onSchedule },
 	{ label="Committees", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onCommittees },
-	{ label="Map", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onMap },
+	{ label="Map", defaultFile="pin.png", overFile="pin-down.png", width = 32, height = 32, onPress=onMap },
 	{ label="Merchandise", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onMerchandise },
 }
 
