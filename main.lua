b8bd927e782e5966db645c3a62805b4a106c023d
Maddy <madelinez820@gmail.com>
2018-01-08 19:55:56 -0500

    asdf

--- main.lua
+++ main.lua
@@ -11,6 +11,9 @@ display.setStatusBar( display.DefaultStatusBar )
 local widget = require "widget"
 local composer = require "composer"
 
+-- more global variables
+local BUTTON_DIMENSION = 32
+local TAB_BAR_HEIGHT = 50
 
 -- event listeners for tab buttons:
 local function onHome( event )
@@ -33,22 +36,23 @@ local function onMerchandise( event )
 	composer.gotoScene( "merchandise" )
 end
 
--- create a tabBar widget with two buttons at the bottom of the screen
+-- create a tabBar widget
 
 -- table to setup buttons
 local tabButtons = {
-	{ label="Home", defaultFile="home.png", overFile="home-down.png", width = 32, height = 32, onPress=onHome, selected=true },
-	{ label="Schedule", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onSchedule },
-	{ label="Committees", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onCommittees },
-	{ label="Map", defaultFile="pin.png", overFile="pin-down.png", width = 32, height = 32, onPress=onMap },
-	{ label="Merchandise", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onMerchandise },
+	{ label="Home", defaultFile="tabbar_1.png", overFile="tabbar_1.png", width = 32, height = 32, onPress=onHome, selected=true },
+	{ label="Schedule", defaultFile="tabbar_2.png", overFile="tabbar_2.png", width = 32, height = 32, onPress=onSchedule },
+	{ label="Committees", defaultFile="tabbar_3.png", overFile="tabbar_3.png", width = 32, height = 32, onPress=onCommittees },
+	{ label="Map", defaultFile="tabbar_4.png", overFile="tabbar_4.png", width = 32, height = 32, onPress=onMap },
+	{ label="Merchandise", defaultFile="tabbar_5.png", overFile="tabbar_5.png", width = 32, height = 32, onPress=onMerchandise },
 }
 
 -- create the actual tabBar widget
 local tabBar = widget.newTabBar{
-	top = display.contentHeight - 50,	-- 50 is default height for tabBar widget
+	top = display.contentHeight - TAB_BAR_HEIGHT,	-- 50 is default height for tabBar widget
 	buttons = tabButtons
 }
+tabBar.height = TAB_BAR_HEIGHT
 
 
-onCommittees()	-- invoke home tab button's onPress event manually
+onHome()	-- invoke home tab button's onPress event manually
